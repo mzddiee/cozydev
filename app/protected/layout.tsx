5d9aba40b4bdc894c6e28fc688befa6975669b72
Maddie <zerios042@gmail.com>
2025-07-28 15:02:44 -0400

    Removed excess imports that prevent Prod

--- app/protected/layout.tsx
+++ app/protected/layout.tsx
@@ -1,7 +1,6 @@
 import { EnvVarWarning } from "@/components/env-var-warning";
 import { AuthButton } from "@/components/auth-button";
 import { hasEnvVars } from "@/lib/utils";
-import Link from "next/link";
 
 export default function ProtectedLayout({
   children,
